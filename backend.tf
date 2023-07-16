d5fc6524fb13fec493dacd4811da609c533c5cb2
Carlos Balcazar <cbalcazarp@gmail.com>
2023-07-15 23:06:59 -0500

    restore provider tf file

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tranzact_RG"
-    storage_account_name = "tranzactcbalcazar4585"
-    container_name       = "tranzactcbalcazar4585container"
-    key                  = "terraform.tfstate"
-  }
-}
--- provider.tf
+++ provider.tf
@@ -8,6 +8,13 @@ terraform {
 
   provider "azurerm" { 
     features {}
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tranzact_RG"
+    storage_account_name = "tranzactcbalcazar4585"
+    container_name       = "tranzactcbalcazar4585container"
+    key                  = "terraform.tfstate"
   }  
 }
 
