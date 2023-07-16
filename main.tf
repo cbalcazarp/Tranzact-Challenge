7bd3a0f13606a98b618fdc868daaad97e1a2c9ca
Carlos Balcazar <cbalcazarp@gmail.com>
2023-07-15 23:21:10 -0500

    moved provider data to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.64.0"
+    }
+  }
+
+  provider "azurerm" { 
+    features {}
+  }
+
+  #backend "azurerm" {
+  #  resource_group_name  = "tranzact_RG"
+  #  storage_account_name = "tranzactcbalcazar4585"
+  #  container_name       = "tranzactcbalcazar4585container"
+  #  key                  = "terraform.tfstate"
+  #}  
+}
+
 module "networking_module" {
   source = "./networking"
   base_name = "tranzact"
