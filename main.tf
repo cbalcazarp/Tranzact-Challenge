bc91f5f8666425be0954a57f1fb4a41c8eb77aef
Carlos Balcazar <cbalcazarp@gmail.com>
2023-07-15 23:37:49 -0500

    modified maint.tf and providers.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  #backend "azurerm" {
-  #  resource_group_name  = "tranzact_RG"
-  #  storage_account_name = "tranzactcbalcazar4585"
-  #  container_name       = "tranzactcbalcazar4585container"
-  #  key                  = "terraform.tfstate"
-  #}
+  backend "azurerm" {
+    resource_group_name  = "tranzact_RG"
+    storage_account_name = "tranzactcbalcazar4585"
+    container_name       = "tranzactcbalcazar4585container"
+    key                  = "terraform.tfstate"
+  }
 }  
 
 module "networking_module" {
